83fcc3687d22304ba8cde3f9c70062fa00f375c8
Thomas McMillan <thomasc.mcmillan@gmail.com>
2024-06-11 02:03:22 +0000

    UDF Changes

--- steps/03_git_config.sql
+++ steps/03_git_config.sql
@@ -19,8 +19,8 @@ USE DATABASE GIT_REPO;
 USE SCHEMA PUBLIC;
 CREATE OR REPLACE SECRET GIT_SECRET 
     TYPE = PASSWORD 
-    USERNAME = '<your_git_user' 
-    PASSWORD = '<your_personal_access_token>';
+    USERNAME = 'tmcmilla' 
+    PASSWORD = 'access token';
 
 --Create an API integration for interacting with the repository API
 USE ROLE ACCOUNTADMIN; 
@@ -29,14 +29,14 @@ USE ROLE GIT_ADMIN;
 
 CREATE OR REPLACE API INTEGRATION GIT_API_INTEGRATION 
     API_PROVIDER = GIT_HTTPS_API 
-    API_ALLOWED_PREFIXES = ('https://github.com/<your_git_user>') 
+    API_ALLOWED_PREFIXES = ('https://github.com/tmcmilla') 
     ALLOWED_AUTHENTICATION_SECRETS = (GIT_SECRET) 
     ENABLED = TRUE;
     
 CREATE OR REPLACE GIT REPOSITORY DE_QUICKSTART 
     API_INTEGRATION = GIT_API_INTEGRATION 
     GIT_CREDENTIALS = GIT_SECRET 
-    ORIGIN = '<your git repo URL ending in .git>';
+    ORIGIN = 'https://github.com/tmcmilla/sfguide-data-engineering-with-snowpark-python-intro.git';
     
 SHOW GIT BRANCHES IN DE_QUICKSTART;
 ls @DE_QUICKSTART/branches/main;
